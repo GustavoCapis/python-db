6f62bad73564b58b2d1da0f510fd424b1841bbdd
Gustavo Nepomuceno Capistrano <gustavo.capy@gmail.com>
2026-04-21 16:13:26 -0300

    feat: add marca as fk for marca(id)

--- ex03.py
+++ ex03.py
@@ -4,14 +4,28 @@ try:
     conexao = conector.connect("./meu_banco.db")
     cursor = conexao.cursor()
     
-    comando = '''CREATE TABLE Veiculo (
-        placa CHARACTER(7) PRIMARY KEY NOT NULL,
-        ano INTEGER  NOT NULL,
-        cor TEXT NOT NULL,
-        proprietario TEXT NOT NULL,
-        marca INTEGER NOT NULL,
-        FOREIGN KEY(proprietario) REFERENCES Pessoa(cpf)
-        )'''
+    comando = '''-- 1. Criar nova tabela
+CREATE TABLE Veiculo_novo (
+    placa CHARACTER(7) PRIMARY KEY NOT NULL,
+    ano INTEGER NOT NULL,
+    cor TEXT NOT NULL,
+    proprietario TEXT NOT NULL,
+    marca INTEGER NOT NULL,
+    modelo INTEGER NOT NULL,
+
+    FOREIGN KEY(proprietario) REFERENCES Pessoa(cpf),
+    FOREIGN KEY(marca) REFERENCES Modelo(id)
+);
+
+-- 2. Copiar dados
+INSERT INTO Veiculo_novo (placa, ano, cor, proprietario, marca)
+SELECT placa, ano, cor, proprietario, marca FROM Veiculo;
+
+-- 3. Apagar antiga
+DROP TABLE Veiculo;
+
+-- 4. Renomear
+ALTER TABLE Veiculo_novo RENAME TO Veiculo;'''
     
     cursor.execute(comando)
     
